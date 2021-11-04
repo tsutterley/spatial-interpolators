fix: modify normalization to prevent high degree overflows

spatial_interpolators/legendre.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 u"""
 legendre.py
-Written by Tyler Sutterley (02/2021)
+Written by Tyler Sutterley (11/2021)
 Computes associated Legendre functions of degree l evaluated for elements x
 l must be a scalar integer and x must contain real values ranging -1 <= x <= 1
 Parallels the MATLAB legendre function
@@ -30,6 +30,8 @@ REFERENCES:
     J. A. Jacobs, "Geomagnetism", Academic Press, 1987, Ch.4.
 
 UPDATE HISTORY:
+    Updated 11/2021: modify normalization to prevent high degree overflows
+    Updated 05/2021: define int/float precision to prevent deprecation warning
     Updated 02/2021: modify case with underflow
     Updated 09/2020: verify dimensions of x variable
     Updated 07/2020: added function docstrings
@@ -38,9 +40,8 @@ UPDATE HISTORY:
     Written 08/2016
 """
 import numpy as np
-import scipy.special
 
-def legendre(l,x,NORMALIZE=False):
+def legendre(l, x, NORMALIZE=False):
     """
     Computes associated Legendre functions of degree l
 
@@ -57,6 +58,8 @@ def legendre(l,x,NORMALIZE=False):
     -------
     Pl: legendre polynomials of degree l for orders 0 to l
     """
+    #-- verify integer
+    l = np.int64(l)
     #-- verify dimensions
     x = np.atleast_1d(x).flatten()
     #-- size of the x array
@@ -64,18 +67,18 @@ def legendre(l,x,NORMALIZE=False):
 
     #-- for the l = 0 case
     if (l == 0):
-        Pl = np.ones((1,nx), dtype=np.float)
+        Pl = np.ones((1,nx), dtype=np.float64)
         return Pl
 
     #-- for all other degrees greater than 0
     rootl = np.sqrt(np.arange(0,2*l+1))#-- +1 to include 2*l
     #-- s is sine of colatitude (cosine of latitude) so that 0 <= s <= 1
     s = np.sqrt(1.0 - x**2)#-- for x=cos(th): s=sin(th)
-    P = np.zeros((l+3,nx), dtype=np.float)
+    P = np.zeros((l+3,nx), dtype=np.float64)
 
     #-- Find values of x,s for which there will be underflow
     sn = (-s)**l
-    tol = np.sqrt(np.finfo(np.float).tiny)
+    tol = np.sqrt(np.finfo(np.float64).tiny)
     count = np.count_nonzero((s > 0) & (np.abs(sn) <= tol))
     if (count > 0):
         ind, = np.nonzero((s > 0) & (np.abs(sn) <= tol))
@@ -83,7 +86,7 @@ def legendre(l,x,NORMALIZE=False):
         v = 9.2 - np.log(tol)/(l*s[ind])
         w = 1.0/np.log(v)
         m1 = 1+l*s[ind]*v*w*(1.0058+ w*(3.819 - w*12.173))
-        m1 = np.where(l < np.floor(m1), l, np.floor(m1)).astype(np.int)
+        m1 = np.where(l < np.floor(m1), l, np.floor(m1)).astype(np.int64)
         #-- Column-by-column recursion
         for k,mm1 in enumerate(m1):
             col = ind[k]
@@ -91,7 +94,7 @@ def legendre(l,x,NORMALIZE=False):
             twocot = -2.0*x[col]/s[col]
             P[mm1-1:l+1,col] = 0.0
             #-- Start recursion with proper sign
-            tstart = np.finfo(np.float).eps
+            tstart = np.finfo(np.float64).eps
             P[mm1-1,col] = np.sign(np.fmod(mm1,2)-0.5)*tstart
             if (x[col] < 0):
                 P[mm1-1,col] = np.sign(np.fmod(l+1,2)-0.5)*tstart
@@ -134,22 +137,20 @@ def legendre(l,x,NORMALIZE=False):
         s0, = np.nonzero(s == 0)
         Pl[0,s0] = x[s0]**l
 
-    #-- Calculate the unnormalized Legendre functions by multiplying each row
-    #-- by: sqrt((l+m)!/(l-m)!) == sqrt(prod(n-m+1:n+m))
-    #-- following Abramowitz and Stegun
-    for m in range(1,l):
-        Pl[m,:] = np.prod(rootl[l-m+1:l+m+1])*Pl[m,:]
-
-    #-- sectoral case (l = m) should be done separately to handle 0!
-    Pl[l,:] = np.prod(rootl[1:])*Pl[l,:]
-
     #-- calculate Fully Normalized Associated Legendre functions
     if NORMALIZE:
         norm = np.zeros((l+1))
         norm[0] = np.sqrt(2.0*l+1)
         m = np.arange(1,l+1)
-        norm[1:] = (-1)**m*np.sqrt(2.0*(2.0*l+1.0)*scipy.special.factorial(l-m)/
-            scipy.special.factorial(l+m))
+        norm[1:] = (-1)**m*np.sqrt(2.0*(2.0*l+1.0))
         Pl *= np.kron(np.ones((1,nx)), norm[:,np.newaxis])
+    else:
+        #-- Calculate the unnormalized Legendre functions by multiplying each row
+        #-- by: sqrt((l+m)!/(l-m)!) == sqrt(prod(n-m+1:n+m))
+        #-- following Abramowitz and Stegun
+        for m in range(1,l):
+            Pl[m,:] *= np.prod(rootl[l-m+1:l+m+1])
+        #-- sectoral case (l = m) should be done separately to handle 0!
+        Pl[l,:] *= np.prod(rootl[1:])
 
     return Pl